60715841b4bdddfdaca70affb176469815c2cbaa
NICOLA-200 <fatepepe66@gmail.com>
2023-12-23 09:27:28 +0200

    first commit

---
+++ src/api/ChatRequests.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+
+const API = axios.create({ baseURL: 'http://localhost:5000' });
+
+export const createChat = (data) => API.post('/chat/', data);
+
+export const deleteChat = (_id) => API.delete('/chat/delete', _id)
+
+export const userChats = (id) => API.get(`/chat/${id}`);
+
+export const findChat = (firstId, secondId) => API.get(`/chat/find/${firstId}/${secondId}`);
+     
