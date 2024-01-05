da053af47fc8f794fcd87e031ec3f859d589dce8
NICOLA-200 <122203222+NICOLA-200@users.noreply.github.com>
2024-01-05 18:55:08 +0200

    Update UploadRequest.js

--- src/api/UploadRequest.js
+++ src/api/UploadRequest.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://social-media-backend-chi.vercel.app" });
+const API = axios.create({ baseURL: "https://backend-art-media.onrender.com" });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
