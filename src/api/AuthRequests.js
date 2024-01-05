49196fd1bd3d2f23fe5d422269ae78fcecd11ce4
NICOLA-200 <122203222+NICOLA-200@users.noreply.github.com>
2024-01-05 12:51:08 +0200

    Update AuthRequests.js
    new

--- src/api/AuthRequests.js
+++ src/api/AuthRequests.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const API = axios.create({ baseURL: 'https://backend-art-media.onrender.com' });
+const API = axios.create({ baseURL: 'https://social-media-backend-chi.vercel.app' });
 
 export const logIn= (formData)=> API.post('/auth/login',formData);
 
