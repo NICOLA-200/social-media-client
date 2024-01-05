22454362d30d67d09da81412a46955f489aba983
NICOLA-200 <122203222+NICOLA-200@users.noreply.github.com>
2024-01-05 12:52:31 +0200

    Update MessageRequests.js
    new

--- src/api/MessageRequests.js
+++ src/api/MessageRequests.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const API = axios.create({ baseURL: 'https://backend-art-media.onrender.com' });
+const API = axios.create({ baseURL: 'https://social-media-backend-chi.vercel.app' });
 
 export const getMessages = (id) => API.get(`/message/${id}`);
 
