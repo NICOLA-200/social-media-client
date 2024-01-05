a9b1346da142aed07fbf6c26918a45ba13c28606
NICOLA-200 <122203222+NICOLA-200@users.noreply.github.com>
2024-01-05 18:59:15 +0200

    Update ChatRequests.js

--- src/api/ChatRequests.js
+++ src/api/ChatRequests.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const API = axios.create({ baseURL: 'https://social-media-backend-chi.vercel.app' });
+const API = axios.create({ baseURL: 'https://backend-art-media.onrender.com' });
 
 export const createChat = (data) => API.post('/chat/', data);
 
